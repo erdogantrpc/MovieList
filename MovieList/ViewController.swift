31e5686cee29a9f1c564d53f1b4073a7da27e692
Erdogan <erdogantrpc@gmail.com>
2023-02-11 14:35:53 +0300

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Erdoğan Turpcu on 11.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
